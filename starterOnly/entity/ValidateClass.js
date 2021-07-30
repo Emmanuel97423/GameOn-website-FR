35abab9065376079a7b8ae2984839aaf565a0683
Emmanuel Narcisse <epokweb@hotmail.fr>
2021-07-30 12:07:58 +0400

    Feature(validate): firstname

---
+++ starterOnly/entity/ValidateClass.js
@@ -0,0 +1,27 @@
+export default  ValidateClass {
+  contructor(form) {
+    this.form = form;
+  }
+
+  get validateFirstName() {
+    return this.checkFirstName;
+  }
+  checkFirstName() {
+    const errorFirstName = document.getElementById("errorFirstName");
+    const first = document.getElementById("first");
+    const myForm = new FormData(this.form);
+    first.addEventListener("input", (e) => {
+      if (myForm.get("first").length < 2) {
+        errorFirstName.innerHTML = "Minimum 2 caractère";
+
+        return false;
+      } else if (!myForm.get("first")) {
+        errorFirstName.innerHTML = "Le champs ne peut pas être vide";
+
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+}
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,5 @@
+//import ValidateClass from "./entity/ValidateClass";
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -11,7 +13,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const form = document.getElementById("form");
+const firstName = document.getElementById("first");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -19,5 +22,65 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+//ecoute du form
+formData.forEach((input) => input.addEventListener("input", launchForm));
 
+function launchForm(e) {
+  const validate = new ValidateClass(form);
+  return validate;
+  const errorFirstName = document.getElementById("errorFirstName");
+  const errorLastName = document.getElementById("errorLastName");
 
+  const myForm = new FormData(form);
+
+  errorFirstName.textContent = myForm.get("first");
+  errorLastName.textContent = myForm.get("last");
+
+  console.log(myForm.get("first"));
+  console.log(myForm.get("last"));
+}
+// launch validate
+
+function validate(e) {
+  e.preventDefault();
+
+  //DOM Elements
+  /* const firstName = document.getElementById("first").value;
+  const lastName = document.getElementById("last").value;
+  const birthDate = document.getElementById("birthdate").value;
+  const checkbox1 = document.getElementById("checkbox1").value;
+
+  const errorLastName = document.getElementById("errorLastName");
+  const errorFirstName = document.getElementById("errorFirstName");
+  const errorBirthDate = document.getElementById("errorBirthDate");
+  const errorCheckbox1 = document.getElementById("errorCheckbox1");
+
+  //First name validation
+
+  //errorFirstName.innerHTML = firstName == "" ? "* Veuillez renseigner votre prénom" : true;
+  if (firstName == "") {
+    errorFirstName.innerHTML = "* Veuillez renseigner votre prénom";
+    return false;
+  } else if (firstName.replace(/\s+/g, "").length < 2) {
+    errorFirstName.innerHTML =
+      "* Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    return false;
+
+    //LastName validation
+  } else if (lastName == "") {
+    errorLastName.innerHTML = "* Veuillez renseigner votre nom";
+    return false;
+  } else if (lastName.replace(/\s+/g, "").length < 2) {
+    errorLastName.innerHTML =
+      "* Veuillez entrer 2 caractères ou plus pour le champ du nom";
+    return false;
+  }
+  //Birthdate validation
+  else if (birthDate == "") {
+    errorBirthDate.innerHTML = "* Vous devez entrer votre date de naissance.";
+    return false;
+  } else {
+    alert("Formulaire soumis");
+    return true;
+  }*/
+}
